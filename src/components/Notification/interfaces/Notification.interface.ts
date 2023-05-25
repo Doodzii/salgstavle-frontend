dfb5d4502c7b2f48d9b581a240b35d130b4537f1
Doodie <alexanderjuhlerkoch@gmail.com>
2023-05-25 11:37:33 +0200

    Removed unecessary Notification component

--- src/components/Notification/interfaces/Notification.interface.ts
+++
@@ -1,8 +0,0 @@
-export type NotificationType = 'success' | 'info' | 'warning' | 'error';
-
-export interface Notification {
-  id: string;
-  title?: string;
-  content: string;
-  type?: NotificationType;
-}
--- src/components/Notification/interfaces/NotificationConfig.interface.ts
+++
@@ -1,9 +0,0 @@
-import type { NotificationType } from "./Notification.interface";
-
-export interface NotificationConfig {
-  title?: string;
-  content: string;
-  duration?: number;
-  autoClose?: boolean;
-  type?: NotificationType;
-}
